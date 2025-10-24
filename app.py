9d7210e087626e766ddcaaea80be928d7eecbd07
viniciusVina <vinidna@hotmail.com>
2025-10-24 15:17:23 -0300

    Impelemntação das telas de confirmação de pedido,pagamento e pedido não encontrado

--- app.py
+++ app.py
@@ -49,12 +49,42 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation',methods=['POST'])
 def pix_confirmation():
+    data = request.get_json()
+    
+    
+    
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message":"invalid payment data"}),400
+    
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+    
+    if not payment or payment.paid:
+        return jsonify({"message":"Paymant not found"}),404
+    
+    if data.get("value") != payment.value:
+        return jsonify({"message":"invalid payment data"}),400
+        
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
     return jsonify({"message":"The payment has been corfirmed"})
+
+
 
 @app.route('/payments/pix/<int:payment_id>',methods=['GET'])
 def payment_px_page(payment_id):
     payment = Payment.query.get(payment_id)
     
+    
+    if not payment:
+        return render_template('404.html')
+    
+    if payment.paid:
+        return render_template('confirmed_payment.html',
+                                payment_id=payment.id, 
+                                value=payment.value,
+                                host="http://127.0.0.1:5000",
+                                qr_code=payment.qr_code_path)
     
     return render_template('payment.html',
                         payment_id=payment.id, 
@@ -67,7 +97,11 @@ def payment_px_page(payment_id):
 @socketio.on('connect')
 def handle_connect():
     print("Cliente conectado ao servidor")
+    
 
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("Cliente desconectado ao servidor")
 
 
 if __name__ == '__main__':
